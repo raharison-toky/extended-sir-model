07188829fd71eb5c6f4ecacbf3fb9de26b104c38
raharison-toky <raharisonrtoky@gmail.com>
2023-01-14 23:19:40 -0500

    edits

--- README.md
+++ README.md
@@ -4,6 +4,15 @@
 - general classes for modeling differential equations problems with Euler's method
 - general classes for finding the coefficients in problems like the SIR models
 
+## How it works
+
+- The graph class can be used to create systems of differential equations that may (or may not) be represented with a graph (like SIR models).
+![image](https://dl.acm.org/cms/attachment/beab90df-5276-4223-9e65-682ac8013686/image1.png)
+
+- The dependent variables like the number of infected individuals can be represented by nodes related to other variables according to the system of differential equations
+
+- Once the graph is created, the model can find the coefficients in the differential equations that give the closest solutions to target data
+
 ## Examples
 
 ![image](firstwave.png)
--- models.py
+++ models.py
@@ -12,13 +12,13 @@ class graph:
 
     """
     Class for differential equation problems that may (or may not) be represented with a graph (like SIR models).
-    Each node contains the evolution of a variable over time and the vertices contain the change for a small dt.
-    Any number of nodes and vertices can be added before the simulation where every vertex is computed.
+    Each node contains the evolution of a variable over time and the edges contain the change for a small dt.
+    Any number of nodes and edges can be added before the simulation where every edge is computed.
     """
 
     def __init__(self) -> None:
         self.nodes = {}
-        self.vertices = {}
+        self.edges = {}
 
     def add_node(self,node):
         """
@@ -31,11 +31,11 @@ class graph:
         else:
             self.nodes[node.name] = node
 
-    def add_vertex(self,start,finish,const,coeffs,name=None):
+    def add_edge(self,start,finish,const,coeffs,name=None):
         """
-        Method that takes in the parameters of a vertex to add a vertex from these parameters to the graph's
-        dictionnary of vertices and return nothing.
-        If a name is not given, a random string will be chosen as the key for that vertex.
+        Method that takes in the parameters of a edge to add a edge from these parameters to the graph's
+        dictionnary of edges and return nothing.
+        If a name is not given, a random string will be chosen as the key for that edge.
         If a starting node, an end node, or coefficients are missing from the graph's nodes, the function will return 0. 
         """
         names = [i.name for i in self.nodes.values()]
@@ -54,13 +54,13 @@ class graph:
 
         else:
             if name is not None:
-                self.vertices[name] = vertex(start,finish,const,coeffs)
+                self.edges[name] = edge(start,finish,const,coeffs)
 
             else:
                 s = "".join(random.choices(ascii_letters,k=10))
-                while s in self.vertices.keys():
+                while s in self.edges.keys():
                     s = "".join(random.choices(ascii_letters,k=10))
-                self.vertices[s] = vertex(start,finish,const,coeffs)
+                self.edges[s] = edge(start,finish,const,coeffs)
 
     def get_values(self,node):
         """
@@ -73,7 +73,7 @@ class graph:
         This is where all the calculations for Euler's method happen.
         Two dictionnaries with the initial values of each node is created.
         The first one is used to get initial values for the calculations, 
-        the second one gets updated by the vertices to get the final values.
+        the second one gets updated by the edges to get the final values.
         After all the calculations have been made, the values in that dictionnary
         are appended to each node of the graph.
         """
@@ -82,8 +82,8 @@ class graph:
         final = {i.name:(i.get_values()[-1] if i.coeff == False else 0)  for i in self.nodes.values()}
         final[None] = 0
 
-        for i in self.vertices.values():
-            # the c is the delta that corresponds to the vertex
+        for i in self.edges.values():
+            # the c is the delta that corresponds to the edge
             c = i.const
             # print(f"c: {c}")
             for j in i.coeffs:
@@ -100,7 +100,7 @@ class graph:
 
     def simulate(self,steps):
         """
-        Method that takes in a number of steps and executes each vertex at every step.
+        Method that takes in a number of steps and executes each edge at every step.
         """
         self.steps = steps
         for i in range(steps):
@@ -142,10 +142,10 @@ class node:
     def get_values(self):
         return self.values
 
-class vertex:
+class edge:
 
     """
-    Class for a vertex that is meant to be added to a graph object.
+    Class for a edge that is meant to be added to a graph object.
     """
 
     def __init__(self,start,finish,const,coeffs) -> None:
@@ -164,18 +164,18 @@ class model:
 
         """
         The init requires a graph object that will be fit to a certain target.
-        The initial_state will contain the starting values of each node and starting constants for vertices.
+        The initial_state will contain the starting values of each node and starting constants for edges.
         """
         
         self.initial_state = copy.deepcopy(initial_state)
-        self.parameters = self.initial_state.vertices
+        self.parameters = self.initial_state.edges
 
-    def fit(self,targets,target_nodes,n_epochs,indices = None,simulate_kwargs=None,start_deltas=None,vertices = []):
+    def fit(self,targets,target_nodes,n_epochs,indices = None,simulate_kwargs=None,start_deltas=None,edges = []):
         """
         This method requires target values, the name of the node for target values, and a number of epochs.
         It first finds an initial loss between the prediction of the initial_state and the target.
-        Then, it creates a copy of the initial_state for every epoch to change the constants of the vertices using gradient descent.
-        After each epoch, the vertices of the best performing graph will be returned.
+        Then, it creates a copy of the initial_state for every epoch to change the constants of the edges using gradient descent.
+        After each epoch, the edges of the best performing graph will be returned.
         If indices are given, the loss will be calculated with the predicted values at these indices.
         A simulate_kwargs can be given so that a child class of graph is given as an initial_state.
         A start_deltas can also be given to choose the starting step for gradient_descent.
@@ -209,15 +209,15 @@ class model:
 
         print(f"starting loss: {loss1}")
 
-        if vertices == []:
-            vertices = new_values.keys()
+        if edges == []:
+            edges = new_values.keys()
         
         for i in range(n_epochs):
             for j in new_values.keys():
                 use_values[j].const += deltas[j]
                 m = copy.deepcopy(self.initial_state)
                 for key,value in use_values.items():
-                    m.vertices[key] = value
+                    m.edges[key] = value
                 
                 m.simulate(*simulate_kwargs)
 
@@ -271,7 +271,7 @@ class general_case:
         """
         This method uses multiprocessing to simulteneously fit multiple starting points.
         It takes in the same arguments as the fit method for a model.
-        It returns the best performing vertices from all the starting points.
+        It returns the best performing edges from all the starting points.
         """
 
         candidates = []
